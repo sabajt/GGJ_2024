d0ae8674d780439bb53fc74a7762a4bdf3613c49
John Saba <john.thomas.saba@gmail.com>
2024-01-27 02:25:45 -0600

    fix

--- main.gd
+++ main.gd
@@ -5,15 +5,16 @@ func _ready() -> void:
 
 func _process(delta) -> void:
 	handle_input(delta)
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("zoom_in"): # up
+		# 36 is a zoom level where we have recuresed into a smaller scene
+		var m = ($Camera2D.zoom / 36) * 30
+		$Camera2D.zoom += Vector2.ONE * m * delta
 		
 func handle_input(delta: float) -> void:
 	if Input.is_action_just_pressed("debug_print"): # p
 		debug_print()
-		
-	if Input.is_action_pressed("zoom_in"): # up
-		# 36 is a zoom level where we have recuresed into a smaller scene
-		var m = ($Camera2D.zoom / 36) * 10
-		$Camera2D.zoom += Vector2(0.1, 0.1) * m
 		
 func debug_print() -> void:
 	print("zoom = %v" % $Camera2D.zoom)
