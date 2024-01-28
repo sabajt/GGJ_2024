72c3511d1500b28b8aa45ca124c3945a74412241
John Saba <john.thomas.saba@gmail.com>
2024-01-28 15:13:28 -0600

    going bak

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var _mouse_position: Vector2
 var _transition_t: float = 0
 var _is_transitioning: bool = false
+var _is_going_back: bool = false
 var _transition_pos: Vector2
 var _transition_id: String
 var _transition_zoom_scale: float
@@ -18,6 +19,11 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float):
 	if _is_transitioning:
 		_process_transition(delta)
+	elif _is_going_back:
+		_go_back()
+		
+func _go_back():
+	_complete_transition()
 		
 func _process_transition(delta: float):
 	_transition_t += 1
@@ -46,22 +52,22 @@ func handle_input(delta: float) -> void:
 		
 func debug_print() -> void:
 	print("zoom = %v" % $Camera2D.zoom)
-
 	print("mouse pos = %v" % _mouse_position)
 	
 func _on_item_selected(id: String, center: Vector2, zoom_scale: float, is_back: bool):
-	_is_transitioning = true
 	_transition_pos = center
-	_transition_id = id
 	_transition_zoom_scale = zoom_scale
 	
-	var manager: GameManager = get_node("/root/GameManager")
-	
+	var manager: GameManager = get_node("/root/GameManager")	
 	if is_back:
 		manager.go_up_path()
+		_transition_id = manager.current_path[-1]
+		_is_going_back = true
 	else:
 		manager.go_down_path(id)
-	
+		_transition_id = id
+		_is_transitioning = true
+		
 	print("item selected %s %v -- zoom scale = %f" % [id, center, zoom_scale])
 
 
